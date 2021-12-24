955ea07008e902687b73a277098aea11a233c866
bora <a52447879@gamil.com>
2021-12-24 17:47:59 +0900

    first commit

---
+++ prepare/front/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppLayout from '../components/AppLayout'
+
+function Home() {
+  return (
+    <div>
+      <AppLayout>
+        <div>Hello, Next!</div>
+        </AppLayout>
+    </div>
+  )
+}
+
+export default Home
