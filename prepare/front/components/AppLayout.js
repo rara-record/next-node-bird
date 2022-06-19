4d6cb146b3b678d62b8975f7cf7b30693e07ff76
bora <a52447879@gmail.com>
2022-06-20 03:39:22 +0900

    [feat] AppLayout 추가

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
-      { children }
+      <div>
+        <Link href="/">
+          <a>노드버드</a>
+        </Link>
+        <Link href="/profile">
+          <a>프로필</a>
+        </Link>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
+      {children}
     </div>
-  )
+  );
 };
 
 AppLayout.propTypes = {
