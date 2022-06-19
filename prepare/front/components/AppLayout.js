7e3fe119b8de92f674db426dd5e0e07d40de91d1
bora <a52447879@gmail.com>
2022-06-20 04:33:19 +0900

    [feat] LoginForm 컴포넌트 추가

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { Menu, Input, Button, Row, Col } from "antd";
 import PropTypes from "prop-types";
+import LoginForm from "./LoginForm";
+import UserProfile from "./UserProfile";
+import { Menu, Input, Button, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -35,7 +39,7 @@ const AppLayout = ({ children }) => {
       */}
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽메뉴
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
@@ -47,6 +51,7 @@ const AppLayout = ({ children }) => {
     </div>
   );
 };
+
 AppLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
