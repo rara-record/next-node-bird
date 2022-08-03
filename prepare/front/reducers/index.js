57bc5ff7f57885c75925863d2d77a552e0ed29c6
kimbora <a52447879@gmail.com>
2022-08-04 06:06:15 +0900

    [feat] redux toolkit 기본 설정

---
+++ prepare/front/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+import { HYDRATE } from "next-redux-wrapper";
+
+import userSlice from "./user";
+import postSlice from "./post";
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      return action.payload;
+    default: {
+      const combinedReducer = combineReducers({
+        user: userSlice.reducer,
+        post: postSlice.reducer,
+      });
+      return combinedReducer(state, action);
+    }
+  }
+};
+
+export default rootReducer;
