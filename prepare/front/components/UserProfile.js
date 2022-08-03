bda78c143b8845f07663ec5f60aa799cc14e0988
kimbora <a52447879@gmail.com>
2022-08-04 04:40:31 +0900

    [feat] UserProfile 추가

--- prepare/front/components/UserProfile.js
+++ prepare/front/components/UserProfile.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useCallback } from "react";
+import Link from "next/link";
 
-const UserProfile = () => {
-  return <div>UserProfile</div>;
+import { Avatar, Button, Card } from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+
+const UserProfile = ({ setIsLoggedIn }) => {
+  const onLogout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  return (
+    <Card
+      actions={[
+        <div key="twit">
+          <Link href="/">
+            <a>
+              게시글
+              <br />1
+            </a>
+          </Link>
+        </div>,
+        <div key="followings">
+          <Link href="/profile">
+            <a>
+              팔로잉
+              <br />2
+            </a>
+          </Link>
+        </div>,
+        <div key="twit">
+          <Link href="/profile">
+            <a>
+              팔로워
+              <br />3
+            </a>
+          </Link>
+        </div>,
+      ]}
+    >
+      <Card.Meta
+        avatar={
+          <Link href="/">
+            <a>
+              <Avatar></Avatar>
+            </a>
+          </Link>
+        }
+        title="보라"
+      />
+      <Button style={{ margin: 15 }} onClick={onLogout}>
+        <LogoutOutlined /> 로그아웃
+      </Button>
+    </Card>
+  );
 };
 
 export default UserProfile;
