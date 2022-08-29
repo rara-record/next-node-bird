f7c47f647b20a7d63cb6293aba93701858d72ca0
bora <a52447879@gmail.com>
2022-08-30 03:02:31 +0900

    [feat] 비로그인 프로필 페이지 접근시 홈으로

--- prepare/front/pages/profile.jsx
+++ prepare/front/pages/profile.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Router from "next/router";
+
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
+import { useSelector } from "react-redux";
 
 function profile() {
+  const { me } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push("/");
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
+
   return (
     <>
       <Head>
